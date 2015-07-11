8710774e11f5c333ddd728d751e74edb6f71ea95
yangarbiter <yangarbiter@gmail.com>
2015-07-11 23:10:30 +0800

    update

--- blending/bagging_blender.py
+++ blending/bagging_blender.py
@@ -9,59 +9,16 @@ from sklearn.datasets import dump_svmlight_file
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.svm import LinearSVC, SVC
 from sklearn.grid_search import GridSearchCV
-from sklearn.isotonic import IsotonicRegression
 from joblib import Parallel, delayed
 
 SVM_PERF_PATH = '/tmp2/b01902066/KDD/kdd15/current/svm_perf'
-#PRED_PATH = '/tmp2/b01902066/KDD/kdd15/blending/allpreds/710'
-PRED_PATH = '/tmp2/kddblending/new_blending1/with_danger/'
+PRED_PATH = '/tmp2/b01902066/KDD/kdd15/blending/allpreds/708'
+#PRED_PATH = '/tmp2/kddblending/new_blending1/with_danger/'
 SVM_RANK_PATH = '/tmp2/b01902066/KDD/kdd15/blending/liblinear-ranksvm-1.95'
 DATA_PATH = '/tmp2/b01902066/KDD/kdd15/blending/data'
 TMPFS_PATH = '/tmp/b01902066'
 
-def perf_SVM_CV(X, y, C, cv, r=0):
-    ret = []
-    train_file = './blend' + str(C) + str(r) + '.svmlight'
-    val_file = './val' + str(C) + str(r) + '.svmlight'
-    model_file = 'blending' + str(C) + str(r) + '.model'
-    preds_file = 'blending_preds' + str(C) + str(r) + '.pred'
-
-    RANKSVM_TRAIN_CMD = SVM_RANK_PATH + '/train ' + '-c ' + str(C) + ' -s 8 '
-    RANKSVM_PRED_CMD = SVM_RANK_PATH + '/predict '
-    if r > 0:
-        RANKSVM_TRAIN_CMD += '-r ' + str(r) + ' '
-        RANKSVM_PRED_CMD += '-r ' + str(r) + ' '
-
-    skf = StratifiedKFold(y, n_folds=cv, shuffle=False)
-    for train_idx, test_idx in skf:
-        trainX = X[train_idx]
-        trainy = y[train_idx]
-        testX = X[test_idx]
-        testy = y[test_idx]
-
-        dump_svmlight_file(trainX, trainy, train_file, zero_based=False)
-        result = subprocess.check_output(
-            RANKSVM_TRAIN_CMD + train_file + ' ' + model_file,
-            shell=True
-        )
-
-        dump_svmlight_file(testX, testy, val_file, zero_based=False)
-        result = subprocess.check_output(
-            RANKSVM_PRED_CMD +
-            val_file + ' ' + model_file + ' ' + preds_file,
-            shell=True
-        )
-
-        preds = []
-        with open(preds_file, 'r') as f:
-            preds = [float(line) for line in f.readlines()]
-
-        ret.append(roc_auc_score(testy, preds))
-
-    return np.mean(ret)
-
 def perf_SVM_train_eval(train_file, val_file, valy, C, r=0):
-    #C = C*len(X)/100.
     model_file = 'blending_' + str(C) + '_' + str(r) + '.model'
     preds_file = 'blending_preds' + str(C) + '_' + str(r) +'.pred'
     RANKSVM_TRAIN_CMD = SVM_RANK_PATH + '/train ' + '-c ' + str(C) + ' -s 8 '
@@ -104,16 +61,15 @@ class Blender():
         candidate_c = [10**i for i in range(-5, 5)]
         res = Parallel(n_jobs=len(candidate_c), backend="threading")(
                     delayed(perf_SVM_train_eval)(
-                        train_file, val_file, valy, c)\
+                        train_file, val_file, valy, C=c)\
                         for c in candidate_c)
         #res = Parallel(n_jobs=len(candidate_c), backend="threading")(
         #            delayed(perf_SVM_CV)(X, y, c, 10) \
         #                for c in candidate_c)
-        print "grid search result: ", res
+        #print "grid search result: ", res
         return max(res), candidate_c[res.index(max(res))]
 
     def removal_select(self, X, y, valX, valy, C):
-        #C = C*len(X)/100.
         train_file = './blend.svmlight'
         val_file = './val.svmlight'
         dump_svmlight_file(X, y, train_file, zero_based=False)
@@ -125,12 +81,6 @@ class Blender():
                     (train_file, val_file, valy, C=C, r=i+1)\
                     for i in candidate_i
             )
-        #res = Parallel(n_jobs=-1, backend="threading")(
-        #        delayed(perf_SVM_CV)\
-        #            (X, y, C, 5, r=i+1)\
-        #            for i in candidate_i
-        #    )
-        #print res
         return max(res), candidate_i[res.index(max(res))]
 
     def addition_select(self, oriX, X, y, orivalX, valX, valy, C):
@@ -149,12 +99,9 @@ class Blender():
         return max(res), candidate_i[res.index(max(res))]
 
     def train(self, X, y, C=1.0, gamma=1.0):
-        #C = C*len(X)/100.
         train_file = './blend.svmlight'
         dump_svmlight_file(X, y, train_file, zero_based=False)
         result = subprocess.check_output(
-                #SVM_PERF_PATH + '/svm_perf_learn' +\
-                #'-c' + str(C) + ' -l 10 -w 3 blend.svmlight blending.model',
                 SVM_RANK_PATH + '/train ' +\
                 '-c ' + str(C) + ' -s 8 ' +\
                 train_file + ' blending.model',
@@ -191,7 +138,7 @@ class Blender():
     def auc_score(self, X, y):
         return roc_auc_score(y, self.clf.predict_proba(X)[:, 1])
 
-def read_preds(path, filelist=[], verbose=True):
+def read_preds(path, filelist=[], verbose=False):
     preds = []
     valpreds = []
     testpreds = []
@@ -213,14 +160,12 @@ def read_preds(path, filelist=[], verbose=True):
                 pred.append(float(row[1]))
         #print(len(pred))
         try:
-            #assert np.shape(pred) == (20000,)
-            assert np.shape(pred) == (19286,)
+            assert np.shape(pred) == (20000,)
+            #assert np.shape(pred) == (19286,)
         except:
             print np.shape(pred)
-        #preds.append(pred)
-        #preds.append(rank_calibrate(pred))
-        preds.append(minmax_calibrate(pred))
-        #preds.append(std_calibrate(pred))
+
+        preds.append(rank_calibrate(pred))
         if verbose:
             print filename
 
@@ -233,10 +178,7 @@ def read_preds(path, filelist=[], verbose=True):
                 if row[0] == 'ID': continue
                 pred.append(float(row[1]))
         assert np.shape(pred) == (24108,)
-        #valpreds.append(pred)
-        #valpreds.append(rank_calibrate(pred))
-        valpreds.append(minmax_calibrate(pred))
-        #valpreds.append(std_calibrate(pred))
+        valpreds.append(rank_calibrate(pred))
 
         test_filename = filename[:-9] + 'test.csv'
         pred = []
@@ -246,10 +188,7 @@ def read_preds(path, filelist=[], verbose=True):
                 if row[0] == 'ID': continue
                 pred.append(float(row[1]))
         assert np.shape(pred) == (80362,)
-        #testpreds.append(pred)
-        #testpreds.append(rank_calibrate(pred))
-        testpreds.append(minmax_calibrate(pred))
-        #testpreds.append(std_calibrate(pred))
+        testpreds.append(rank_calibrate(pred))
 
     return np.array(preds).T, np.array(valpreds).T, np.array(testpreds).T, filelist
 
@@ -259,25 +198,11 @@ def rank_calibrate(x):
     ranks = order.argsort()
     return ranks / np.float(len(ranks))
 
-def minmax_calibrate(x):
-    return (x - np.min(x)) / (np.max(x) - np.min(x))
-    #t = np.log(((x - np.min(x)) / (np.max(x) - np.min(x)))+1)
-    #return ((t - np.min(t)) / (np.max(t) - np.min(t)))
-
-def std_calibrate(x):
-    return (x - np.mean(x)) / np.var(x)
-
-def isotonic_calibrate(x):
-    return (x - np.mean(x)) / np.var(x)
-
 def read_truth():
-    #with open('/tmp2/b01902066/KDD/kdd15/current/data/0610/label_train+blend+valid.npy', 'rb') as f:
-    #    y = np.load(f)
-    #return y
     eid = []
     y = []
-    #with open(DATA_PATH + '/truth_blend.csv', 'rb') as csvfile:
-    with open('/tmp2/kddblending/new_blend_data1/blend1/truth_test.csv', 'rb') as csvfile:
+    with open(DATA_PATH + '/truth_blend.csv', 'rb') as csvfile:
+    #with open('/tmp2/kddblending/new_blend_data1/blend1/truth_test.csv', 'rb') as csvfile:
         reader = csv.reader(csvfile, delimiter=",")
         for row in reader:
             eid.append(int(row[0]))
@@ -285,8 +210,8 @@ def read_truth():
 
     testeid = []
     testy = []
-    #with open(DATA_PATH + '/internal1/truth_test.csv', 'rb') as csvfile:
-    with open('/tmp2/kddblending/new_blend_data1/valid1/truth_test.csv', 'rb') as csvfile:
+    with open(DATA_PATH + '/internal1/truth_test.csv', 'rb') as csvfile:
+    #with open('/tmp2/kddblending/new_blend_data1/valid1/truth_test.csv', 'rb') as csvfile:
         reader = csv.reader(csvfile, delimiter=",")
         for row in reader:
             testeid.append(int(row[0]))
@@ -317,7 +242,6 @@ def outputans(ans, id_file_path, path):
 
 def parse_exp_result(res):
     acc = []
-    #print res.split('\n')
     for line in res.split('\n')[:-1]:
         acc.append(float(line.split(':')[1]))
     return acc
@@ -333,47 +257,18 @@ def main():
         removed = [filename.strip() for filename in f.readlines()]
 
     for filename in sorted(os.listdir(PRED_PATH)):
-        if '_blend.csv' in filename and filename not in removed and 'team3' not in filename:
+        if '_blend.csv' in filename and filename not in removed:
             filelist.append(filename)
+
+    sample_idx = np.random.randint(0, 2, (len(filelist)))
+    temp = []
+    for i, r in enumerate(sample_idx):
+        if r == 1:
+            temp.append(filelist[i])
+    filelist = temp
 
     eid, y, valeid, valy = read_truth()
     X, valX, testX, filelist = read_preds(PRED_PATH, filelist)
-
-    #trainX = []
-    #trainy = []
-    #for filename in filelist:
-    #    x = []
-    #    with open('/tmp2/kddtrainpred/' + filename[6:-4] + '_train.csv', 'rb') as csvfile:
-    #        reader = csv.reader(csvfile, delimiter=",")
-    #        for row in reader:
-    #            x.append(float(row[1]))
-    #    trainX.append(minmax_calibrate(x))
-
-    #with open('/tmp2/kdd/truth_train.csv', 'rb') as csvfile:
-    #    reader = csv.reader(csvfile, delimiter=",")
-    #    for row in reader:
-    #        trainy.append(float(row[1]))
-
-    #trainX = np.array(trainX).T
-    #trainy = np.array(trainy).T
-
-    #print trainX.shape
-    #print trainy.shape
-
-    #for i in range(X.shape[1]):
-    #    iso = IsotonicRegression()
-    #    iso.fit(trainX[:, i], trainy)
-    #    X[:, i] = iso.transform(X[:, i])
-    #    valX[:, i] = iso.transform(valX[:, i])
-
-
-    #print X[0].tolist()
-
-    #X = np.hstack((X, X**2))
-    #valX = np.hstack((valX, valX**2))
-    #testX = np.hstack((testX, testX**2))
-
-    print np.shape(X), np.shape(valX), np.shape(testX)
 
     clf = Blender()
     #print "------------------"
@@ -397,11 +292,11 @@ def main():
 
     score, C = clf.grid_search(X, y, valX, valy)
     #C = 0.00001
-    print 'C:', C
-    print 'auc val:', score
-    print "------------------"
-    clf.train(valX, valy, C)
-    print roc_auc_score(y, clf.predict(X, y))
+    #print 'C:', C
+    #print 'auc val:', score
+    #print "------------------"
+    #clf.train(valX, valy, C)
+    #print roc_auc_score(y, clf.predict(X, y))
 
     #clf.train(np.vstack((X, valX)), np.hstack((y, valy)), C)
 
@@ -416,8 +311,7 @@ def main():
 
     best = -np.inf
     score = -1
-    #while score > best:
-    for k in range(0, np.shape(X)[1]):
+    while score > best:
         best = score
         score, i = clf.removal_select(X, y, valX, valy, C)
         print 'delete i:', filelist[i], 'auc val:', score
@@ -426,74 +320,17 @@ def main():
         valX = np.delete(valX, [i], 1)
         testX = np.delete(testX, [i], 1)
         del filelist[i]
-
-    #idxs = []
-    #for k in range(np.shape(X)[1], -1, -1):
-    #    score, i = clf.addition_select(X, y, valX, valy, C, k)
-    #    print 'added i:', filelist[i], 'auc val:', score
-
-    #    X = np.append(X, X[:,i])
-    #    valX = np.append(valX, valX[:,i])
-    #    testX = np.append(testX, testX[:,i])
-
-    #    X = np.delete(X, [i], 1)
-    #    valX = np.delete(valX, [i], 1)
-    #    testX = np.delete(testX, [i], 1)
-    #    del filelist[i]
-
+    print ''
+    exit()
 
     clf.train(np.vstack((X, valX)), np.hstack((y, valy)), C)
     dump_svmlight_file(testX, np.zeros((len(testX))), 'test.svmlight', zero_based=False)
     result = subprocess.check_output(
-        #SVM_PERF_PATH + '/svm_perf_classify test.svmlight ./blending.model ./blending_prediction',
         SVM_RANK_PATH + '/predict test.svmlight ./blending.model ./blending_prediction',
         shell=True
     )
     outputans([], DATA_PATH + '/enrollment_test.csv', '')
     exit()
-
-    BLENDER_PATH = os.path.dirname(os.path.abspath(__file__))
-    print BLENDER_PATH
-
-    if len(sys.argv) == 1:
-        #os.chdir('./exp')
-        os.chdir(TMPFS_PATH + '/exp')
-        for i in range(1):
-            result = subprocess.check_output(
-                    './run_exps.sh ' + str(len(X[0])-i) + ' ' +\
-                    BLENDER_PATH + '/blender.py',
-                    #'/tmp2/b01902066/blending/blender.py',
-                    shell=True
-                )
-            result = subprocess.check_output(
-                    './view_result.sh ' + str(len(X[0])-i),
-                    shell=True
-                )
-            res = parse_exp_result(result)
-            #with open(BLENDER_PATH + '/blending.model', 'r') as f:
-            #    w = f.readlines()[-1].split()[1:-1]
-            ind = np.argsort(res)[::-1]
-            #print res, w
-            for i in ind:
-                print (filelist[i], res[i])#, w[i].split(':')[1])
-            print ''
-            #print 'remove:', filelist[ind[0]]
-            #print 'max auc:', res[ind[0]]
-            #os.remove(PRED_PATH + filelist[ind[0]])
-            del filelist[ind[0]]
-        #os.chdir(TMPFS_PATH + 'blending/')
-        os.chdir(os.path.dirname(__file__))
-
-        #dump_svmlight_file(testX, np.zeros((len(testX))), 'test.svmlight', zero_based=False)
-        #result = subprocess.check_output(
-        #    SVM_PERF_PATH+'svm_perf_classify test.svmlight ./blending.model ./blending_prediction',
-        #    shell=True
-        #)
-        #outputans([], '/tmp2/b01902066/KDD/data/enrollment_test.csv', '')
-
-    #outputans(clf.predict(testX)[:, 1],
-    #        '/tmp2/b01902066/KDD/data/enrollment_test.csv',
-    #        '610_gbm_gbmrank_rf_ada_polylog_blend_logist_ans.csv')
 
 if __name__ == "__main__":
     main()
